9a6af085bfa8845cda3528ab26817201df3a1bc1
mikezks <michael.zikes@intauria.com>
2018-11-26 15:25:16 +0100

    JS

--- src/basic-html/main.js
+++ src/basic-html/main.js
@@ -8,3 +8,10 @@ var myVariable = 'Hello World!';
 console.log(myVariable);
 myVariable = 9;
 console.log(myVariable);
+
+document.getElementById('js-output').innerHTML = 'my javascript value';
+
+var resultFn = add;
+var result = add(1, 2);
+
+console.log(resultFn(5, 4));
