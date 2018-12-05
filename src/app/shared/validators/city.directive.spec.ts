ff2a38c7cae22dd00be8e312401685e93f054a62
mikezks <michael.zikes@intauria.com>
2018-12-05 20:04:14 +0100

    Testing

--- src/app/shared/validators/city.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CityDirective } from './city.directive';
-
-describe('CityDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CityDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/shared/validators/round-trip.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RoundTripDirective } from './round-trip.directive';
-
-describe('RoundTripDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RoundTripDirective();
-    expect(directive).toBeTruthy();
-  });
-});
